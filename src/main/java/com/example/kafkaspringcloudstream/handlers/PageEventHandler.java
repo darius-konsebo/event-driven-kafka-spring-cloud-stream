1321816a411d48dc63250911b27eb5887cd033f7
darius-konsebo <dariuskonsebo@gmail.com>
2025-10-18 12:14:36 +0100

    Service de Data Analytics Real Time Stream Processing avec Kaflka Streams

--- src/main/java/com/example/kafkaspringcloudstream/handlers/PageEventHandler.java
+++ src/main/java/com/example/kafkaspringcloudstream/handlers/PageEventHandler.java
@@ -1,12 +1,19 @@
 package com.example.kafkaspringcloudstream.handlers;
 
 import com.example.kafkaspringcloudstream.events.PageEvent;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.kstream.Grouped;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.TimeWindows;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
+import java.time.Duration;
 import java.util.Date;
 import java.util.Random;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Supplier;
 
 @Component
@@ -30,5 +37,18 @@ public class PageEventHandler {
                     10+ new Random().nextInt(10000)
                     );
         };
+    }
+
+    @Bean
+    public Function<KStream<String, PageEvent>, KStream<String, Long>> kStreamFunction(){
+        return (input)->
+                input.filter((k,v)->v.duration()>100)
+                        .map((k,v)->new KeyValue<>(v.name(), v.duration()))
+                        .groupByKey(Grouped.with(Serdes.String(), Serdes.Long()))
+                        .windowedBy(TimeWindows.of(Duration.ofSeconds(5000)))
+                        .count()
+                        .toStream()
+                        .map((k,v)->new KeyValue<>(k.key(), v))
+                ;
     }
 }
