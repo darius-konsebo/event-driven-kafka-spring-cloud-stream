d24483ebb869891d1c7e31f61d0c8ff948c49c53
darius-konsebo <dariuskonsebo@gmail.com>
2025-10-18 11:12:15 +0100

    Service Supplier KAFKA

--- src/main/java/com/example/kafkaspringcloudstream/handlers/PageEventHandler.java
+++ src/main/java/com/example/kafkaspringcloudstream/handlers/PageEventHandler.java
@@ -4,7 +4,10 @@ import com.example.kafkaspringcloudstream.events.PageEvent;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
+import java.util.Date;
+import java.util.Random;
 import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 @Component
 public class PageEventHandler {
@@ -14,6 +17,18 @@ public class PageEventHandler {
             System.out.println("**********");
             System.out.println(input.toString());
             System.out.println("**********");
+        };
+    }
+
+    @Bean
+    public Supplier<PageEvent> pageEventSupplier(){
+        return ()->{
+            return new PageEvent(
+                    Math.random()>0.5?"P1":"P2",
+                    Math.random()>0.5?"U1":"U2",
+                    new Date(),
+                    10+ new Random().nextInt(10000)
+                    );
         };
     }
 }
